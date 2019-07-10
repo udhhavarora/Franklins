abe94058151a4f6708dcaaf4376325e19dd512e2
Udhhav Arora <udhhavarora@gmail.com>
2019-07-10 15:32:14 +0530

    Update contact.php

--- Frankie/contact.php
+++ Frankie/contact.php
@@ -25,7 +25,7 @@
 
 		<?php
 	
-		// Check for Header Injections
+		
 		function has_header_injection($str) {
 			return preg_match( "/[\r\n]/", $str );
 		}
@@ -33,13 +33,12 @@
 		
 		if (isset($_POST['contact_submit'])) {
 			
-			// Assign trimmed form data to variables
-			// Note that the value within the $_POST array is looking for the HTML "name" attribute, i.e. name="email"
+			
 			$name	= trim($_POST['name']);
 			$email	= trim($_POST['email']);
 			$msg	= $_POST['message']; // no need to trim message
 		
-			// Check to see if $name or $email have header injections
+			
 			if (has_header_injection($name) || has_header_injection($email)) {
 				
 				die(); // If true, kill the script
@@ -51,28 +50,28 @@
 				exit;
 			}
 			
-			// Add the recipient email to a variable
+			
 			$to	= "udhhavarora@gmail.com";
 			
-			// Create a subject
+			
 			$subject = "$name sent a message to Frankie's.";
 			
-			// Construct the message
+			
 			$message .= "Name: $name\r\n";
 			$message .= "Email: $email\r\n\r\n";
 			$message .= "Message:\r\n$msg";
 			
-			// If the subscribe checkbox was checked
+			
 			if (isset($_POST['subscribe']) && $_POST['subscribe'] == 'Subscribe' ) {
 			
-				// Add a new line to the $message
+				
 				$message .= "\r\n\r\nPlease add $email to the mailing list.\r\n";
 				
 			}
 		
-			$message = wordwrap($message, 72); // Keep the message neat n' tidy
+			$message = wordwrap($message, 72); 
 		
-			// Set the mail headers into a variable
+			
 			$headers = "MIME-Version: 1.0\r\n";
 			$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
 			$headers .= "From: " . $name . " <" . $email . ">\r\n";
@@ -80,11 +79,11 @@
 			$headers .= "X-MSMail-Priority: High\r\n\r\n";
 		
 			
-			// Send the email!
+			
 			mail($to, $subject, $message, $headers);
 		?>
 		
-		<!-- Show success message after email has sent -->
+		
 		<h5>Thanks for contacting Franklin's!</h5>
 		<p>Please allow 24 hours for a response.</p>
 		<p><a href="index.php" class="button block">&laquo; Go to Home Page</a></p>
